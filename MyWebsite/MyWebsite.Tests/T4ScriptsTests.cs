ba4dd3da2682f1f8366e1c8bf497efbfd7fd2647
John Wu <me@johnwu.cc>
2017-09-11 23:58:21 +0800

    Update Unit Test

--- MyWebsite/MyWebsite.Tests/T4ScriptsTests.cs
+++ MyWebsite/MyWebsite.Tests/T4ScriptsTests.cs
@@ -2,52 +2,45 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Resources;
-using System.Runtime.Loader;
-using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using NUnit.Framework;
+using Resources;
 
 namespace MyWebsite.Tests
 {
     [TestFixture]
     public class T4ScriptsTests
     {
-        private static string _assemblyPath = Directory.GetCurrentDirectory();
-
         [Test]
-        public void T4_LoadResourceManager()
+        [TestCase(new object[] { "Text", "Hello", "en-gb", "Hello~ This message from Text.en-GB.resx" })]
+        [TestCase(new object[] { "Text", "Hello", "zh-tw", "您好~ 這段文字來自 Text.zh-TW.resx" })]
+        public void T4_GetResourcesByCulture(string category, string resourceKey, string culture, string expected)
         {
             // Arrange
-            var expected = "Hello~ This message from Text.en-GB.resx";
-            var culture = "en-gb";
-            var category = "Text";
-            var key = "Hello";
+            var resourceFolder = "Resources";
+            var files = Directory.GetFiles(resourceFolder, "*.resx");
 
             // Act
-            var actual = LoadResourceManager()[$"{category}.{culture}"].GetString(key);
+            var actual = GetResourcesByCulture(culture, resourceFolder)[category.ToString()][resourceKey];
 
             // Assert
             Assert.AreEqual(expected, actual);
         }
 
-        private Dictionary<string, ResourceManager> LoadResourceManager()
+        private Dictionary<string, Dictionary<string, string>> GetResourcesByCulture(string culture, string resourceFolder)
         {
-            var directory = Path.GetDirectoryName(_assemblyPath);
-            var files = Directory.GetFiles(directory, "*.resources.dll", SearchOption.AllDirectories);
-
-            var resources = new Dictionary<string, ResourceManager>(StringComparer.CurrentCultureIgnoreCase);
-            foreach (var file in files)
+            var files = Directory.GetFiles(resourceFolder, "*.resx");
+            var resources = files.GroupBy(file =>
             {
-                var culture = Path.GetFileName(Path.GetDirectoryName(file));
-                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
-                foreach (var resourceName in assembly.GetManifestResourceNames().Select(s => Regex.Replace(s, ".resources$", "")))
-                {
-                    var category = Regex.Match(resourceName, $".*Resources\\.(.*)\\.{culture}").Groups[1].Value;
-                    var resourceManager = new ResourceManager(resourceName, assembly);
-                    resources.Add($"{category}.{culture}", resourceManager);
-                }
-            }
-
+                var fileName = Path.GetFileNameWithoutExtension(file).Split('.');
+                return fileName.First();
+            }).ToDictionary(g => g.Key, g =>
+            {
+                var defaultFile = g.Single(s => s.IndexOf(culture, StringComparison.CurrentCultureIgnoreCase) != -1);
+                var xdoc = XDocument.Load(defaultFile);
+                var dictionary = xdoc.Root.Elements("data").ToDictionary(e => e.Attribute("name").Value, e => e.Element("value").Value);
+                return dictionary;
+            });
             return resources;
         }
     }
